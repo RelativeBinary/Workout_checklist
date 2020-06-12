877ba083fad52413a862262714cd2a7757d2045f
PatrickBornay <patrick.bornay@gmail.com>
2020-06-12 15:13:14 +1000

    added navbar component to app:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <Navbar />
     </div>
   );
 }
