97239ec9c2ca72b5ff4ab52151096cf124f8bbba
PatrickBornay <patrick.bornay@gmail.com>
2020-06-12 15:18:41 +1000

    added newworkoutform component to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import WorkoutContextProvider from './contexts/WorkoutContext';
 import Navbar from './components/Navbar';
 import WorkoutList from './components/WorkoutList';
+import NewWorkoutForm from './components/WorkoutForm';
 function App() {
   return (
     <div className='App'>
       <WorkoutContextProvider>
         <Navbar />
         <WorkoutList />
+        <NewWorkoutForm />
       </WorkoutContextProvider>
     </div>
   );
